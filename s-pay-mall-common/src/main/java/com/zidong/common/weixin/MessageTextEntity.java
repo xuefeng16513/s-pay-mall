a66fb33d95a1d2ca7c3ed506c3c2f5cce7a0c9dd
XuZidong <3486474753@qq.com>
2025-07-22 15:04:35 -0400

    修改get，set方法实现

--- s-pay-mall-common/src/main/java/com/zidong/common/weixin/MessageTextEntity.java
+++ s-pay-mall-common/src/main/java/com/zidong/common/weixin/MessageTextEntity.java
@@ -1,10 +1,15 @@
 package com.zidong.common.weixin;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @XStreamAlias("xml")
 public class MessageTextEntity {
 
+    // Getters and Setters
     @XStreamAlias("ToUserName")
     private String toUserName;
 
@@ -35,84 +40,4 @@ public class MessageTextEntity {
     @XStreamAlias("Content")
     private String content;
 
-    // Getters and Setters
-    public String getToUserName() {
-        return toUserName;
-    }
-
-    public void setToUserName(String toUserName) {
-        this.toUserName = toUserName;
-    }
-
-    public String getFromUserName() {
-        return fromUserName;
-    }
-
-    public void setFromUserName(String fromUserName) {
-        this.fromUserName = fromUserName;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getMsgType() {
-        return msgType;
-    }
-
-    public void setMsgType(String msgType) {
-        this.msgType = msgType;
-    }
-
-    public String getEvent() {
-        return event;
-    }
-
-    public void setEvent(String event) {
-        this.event = event;
-    }
-
-    public String getMsgId() {
-        return msgId;
-    }
-
-    public void setMsgId(String msgId) {
-        this.msgId = msgId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getEventKey() {
-        return eventKey;
-    }
-
-    public void setEventKey(String eventKey) {
-        this.eventKey = eventKey;
-    }
-
-    public String getTicket() {
-        return ticket;
-    }
-
-    public void setTicket(String ticket) {
-        this.ticket = ticket;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
